3bc430bf6db02ce3dc0513f67b9145080a4e0d49
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-01-21 14:08:39 +0800

    Initial commit

---
+++ src/main/java/com/besson/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.besson.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
