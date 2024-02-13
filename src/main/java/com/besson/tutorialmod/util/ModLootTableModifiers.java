2b7e7e3f6c53e4a5f0b360124e1cfa576a25e600
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-02-13 21:46:44 +0800

    modifyingSusSandLootTables

--- src/main/java/com/besson/tutorialmod/util/ModLootTableModifiers.java
+++ src/main/java/com/besson/tutorialmod/util/ModLootTableModifiers.java
@@ -3,6 +3,7 @@ package com.besson.tutorialmod.util;
 import com.besson.tutorialmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
@@ -13,6 +14,8 @@ import net.minecraft.util.Identifier;
 public class ModLootTableModifiers {
     private static final Identifier JUNGLE_TEMPLE_ID =
             new Identifier("minecraft","chests/jungle_temple");
+    private static final Identifier SUS_SAND_ID =
+            new Identifier("minecraft","archaeology/desert_pyramid");
     public static void modifierLootTables(){
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             if (JUNGLE_TEMPLE_ID.equals(id)){
@@ -42,6 +45,13 @@ public class ModLootTableModifiers {
 
                 tableBuilder.pool(poolBuilder.build());
             }
+        });
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
+            if (SUS_SAND_ID.equals(id)){
+                LootPool.Builder pool = LootPool.builder().with(ItemEntry.builder(ModItems.PROSPECTOR)).with(ItemEntry.builder(ModItems.RAW_ICE_ETHER));
+                return LootTable.builder().pool(pool).build();
+            }
+            return null;
         });
     }
 }
